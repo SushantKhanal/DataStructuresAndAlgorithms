15302f0c10cf950bce79b84be5a4a58eff5112ec
SushantKhanal <dreamerking114@gmail.com>
2023-08-14 20:54:27 -0500

    sliding window plus hash map solution

--- .vscode/dsa5.js
+++ .vscode/dsa5.js
@@ -9,3 +9,35 @@
 
 // Input: nums = [1,5,4,2,9,9,9], k = 3
 // Output: 15
+
+var maximumSubarraySum = function(nums, k) {
+    let windowStart = 0;
+    let currSum = 0; let maxSum = 0;
+    const windowMap = {};
+    let windowUniqueLen = 0;
+    for(let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+        const currNum = nums[windowEnd];
+        currSum += currNum;
+        if(!(currNum in windowMap)) {
+            windowMap[currNum] = 1;
+            windowUniqueLen++;
+        }
+        else windowMap[currNum]++;
+
+        if(windowEnd >= k) {
+            windowMap[nums[windowStart]]--;
+            if(windowMap[nums[windowStart]] <= 0) {
+                delete windowMap[nums[windowStart]];
+                windowUniqueLen--;
+            }
+            currSum -= nums[windowStart];
+            windowStart++;
+        }
+
+        if(windowUniqueLen === k) {
+            maxSum = Math.max(maxSum, currSum); 
+        }
+    }
+    return maxSum;
+};
+
