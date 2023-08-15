02df06cfca52d972efd3840a8bcecc62e6160044
SushantKhanal <dreamerking114@gmail.com>
2023-08-14 20:56:13 -0500

    209. answer

--- .vscode/dsa6.js
+++ .vscode/dsa6.js
@@ -4,3 +4,16 @@
 // subarray
 //  whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
 
+var minSubArrayLen = function(target, nums) {
+    let windowStart = 0;
+    let currSum = 0; let minLen = Infinity;
+    for(let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
+        currSum += nums[windowEnd];
+        while(currSum >= target) {
+            minLen = Math.min(windowEnd - windowStart + 1, minLen);
+            currSum -= nums[windowStart];
+            windowStart++;
+        }
+    }
+    return (minLen === Infinity) ? 0 : minLen;
+};
