8786823816b8f98a4dbd67a3002cc99874290aa5
SushantKhanal <dreamerking114@gmail.com>
2023-09-02 22:41:56 -0500

    sliding window solution

--- .vscode/dsa7.js
+++ .vscode/dsa7.js
@@ -1,18 +1,8 @@
 // 340. Longest Substring with At Most K Distinct Characters
 // Medium
-// 2.7K
-// 78
-// company
-// Yandex
-// company
-// Amazon
-// company
-// Google
 // Given a string s and an integer k, return the length of the longest 
 // substring
 //  of s that contains at most k distinct characters.
-
- 
 
 // Example 1:
 
@@ -24,4 +14,25 @@
 // Input: s = "aa", k = 1
 // Output: 2
 // Explanation: The substring is "aa" with length 2.
+
+var lengthOfLongestSubstringKDistinct = function(s, k) {
+    const charCountMap = {}; let windowStart = 0; let distinctCharsLen = 0;
+    let substringLen = -Infinity;
+    for(let windowEnd = 0; windowEnd < s.length; windowEnd++) {
+        if(!(s[windowEnd] in charCountMap)) {
+            charCountMap[s[windowEnd]] = 1;
+            distinctCharsLen++;
+        } else charCountMap[s[windowEnd]]++;
+        while(distinctCharsLen > k) {
+            charCountMap[s[windowStart]]--;
+            if(charCountMap[s[windowStart]] === 0) {
+                delete charCountMap[s[windowStart]];
+                distinctCharsLen--;
+            } 
+            windowStart++;
+        }
+        substringLen = Math.max(substringLen, (windowEnd - windowStart) + 1);
+    }
+    return substringLen;
+};
 
