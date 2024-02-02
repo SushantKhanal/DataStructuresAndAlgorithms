d2369c0c71addcf3c73faa63e76353db02109364
SushantKhanal <dreamerking114@gmail.com>
2024-02-02 12:48:52 -0800

    refactor

--- OddEvenPrintThread2.java
+++ OddEvenPrintThread2.java
@@ -1,5 +1,4 @@
 //odd even print implementation with future object
-import java.util.function.IntPredicate;
 import java.util.concurrent.CompletableFuture;
 import java.util.stream.IntStream;
 
@@ -8,10 +7,6 @@ import java.util.concurrent.Executors;
 
 
 public class OddEvenPrintThread2 {
-    private static Object object = new Object();
-    private static IntPredicate evenCondition = (i) -> (i % 2) == 0;
-    private static IntPredicate oddCondition = (i) -> (i % 2) == 1;
-
     public static void main(String[] args) {
         ExecutorService executorService = Executors.newFixedThreadPool(2);
         IntStream.rangeClosed(1, 10)
