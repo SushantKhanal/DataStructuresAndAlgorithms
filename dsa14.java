54f23309ebc04b9ea28e955d1e963b957a36298d
SushantKhanal <dreamerking114@gmail.com>
2024-02-13 18:51:04 -0800

    solution added

--- dsa14.java
+++ dsa14.java
@@ -9,3 +9,44 @@
 // return the index of target if it is in nums, or -1 if it is not in nums.
 // You must write an algorithm with O(log n) runtime complexity.
 
+//Soln:
+
+class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length == 0) return -1;
+        if(nums.length == 1) return (nums[0] == target) ? 0 : -1;
+        int minIdx = getMinIdx(nums);
+        if(minIdx < 0) return -1;
+        int ans = searchHelper(nums, 0, minIdx - 1, target); // Search in the first half
+        if(ans != -1) return ans; // If found in the first half, return
+        ans = searchHelper(nums, minIdx, nums.length - 1, target); // Otherwise, search in the second half
+        return ans; // Return -1 if not found, otherwise return the found index
+    }
+    
+    public int searchHelper(int[] nums, int low, int high, int target) {
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid; // Found target
+            } else if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1; // Target not found
+    }
+    
+    public int getMinIdx(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left; // This is the index of the smallest element
+    }
+}
